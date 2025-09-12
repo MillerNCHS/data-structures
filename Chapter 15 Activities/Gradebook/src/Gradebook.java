d01521311607398faf806c8528ff1e2cbfbcb96c
shriyak26 <skunnanath1@stu.naperville203.org>
2025-09-12 12:30:23 -0500

    Update Gradebook.java

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-. . .
+import java.util.Set;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,33 +13,53 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> gradebook = new HashMap<>();
+      //Set<String> keys = gradebook.keySet();
+       
 
         boolean done = false;
         while(!done)
         {
             System.out.println("A)dd R)emove M)odify P)rint Q)uit");
+        
             String input = in.next().toUpperCase();
             if (input.equals("Q"))
             {
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                String name = in.next();
+                String grade = in.next();
+                gradebook.put(name, grade);
 
             } else if (input.equals("R"))
             {
-                . . .
-            } else if (input.equals("M"))
+                String name = in.next();
+
+                Set<String> keys = gradebook.keySet();
+                 for (String key: keys) {
+                    if (key==name){
+                        gradebook.remove(key);}
+            }}else if (input.equals("M"))
             {
-                . . .
-            } else if (input.equalsIgnoreCase("P"))
+                String name = in.next();
+                String grade = in.next();
+
+                Set<String> list=gradebook.keySet();
+                for (String key: list){
+                    if (key.equals(name)){
+                        gradebook.put(name, grade);
+
+            }else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> x = gradebook.keySet();
+                 for (String i: x) {
+                    System.out.println(i + ": "+gradebook.get(i));
+        }
             } else
             {
                 done = true;
             }
         }
     }
-}
+        }}}
